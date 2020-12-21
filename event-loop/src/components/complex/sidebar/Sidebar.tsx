6a14530bf59d91e05a6235044d20d9e75cc9dc8f
Zoltan Szabo <ZoltanZ.Szabo@endava.com>
2020-12-21 17:21:59 +0200

    [event-loop] keydown event

--- event-loop/src/components/complex/sidebar/Sidebar.tsx
+++ event-loop/src/components/complex/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { ACTIONS, IState } from '../../../store';
@@ -11,6 +11,35 @@ const Sidebar = () => {
   const step = useSelector((state: IState) => state.step);
 
   const dispatch = useDispatch();
+
+  const nextStep = useCallback(() => {
+    dispatch({
+      type: ACTIONS.setStep,
+      payload: step < steps.length - 1 ? step + 1 : step,
+    });
+  }, [dispatch, step]);
+
+  const prevStep = useCallback(() => {
+    dispatch({
+      type: ACTIONS.setStep,
+      payload: step > 0 - 1 ? step - 1 : step,
+    });
+  }, [dispatch, step]);
+
+  const keyDown = useCallback(
+    (e: KeyboardEvent) => {
+      e.code === 'ArrowDown' && nextStep();
+      e.code === 'ArrowUp' && prevStep();
+    },
+    [prevStep, nextStep]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDown);
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+    };
+  }, [keyDown]);
 
   const handleClick = (index: number) => {
     dispatch({ type: ACTIONS.setStep, payload: index });
